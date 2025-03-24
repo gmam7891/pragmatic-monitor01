96d2f89903a5d18c867e2ffd080549ca42050ff8
gmam7891 <guilhermemontanari@gmail.com>
2025-03-24 18:11:34 -0300

    app.py

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def rotina_agendada():
     enviar_alerta_email(todos)
 
 def iniciar_agendamento():
-    schedule.every(1).hours.do(rotina_agendada)
+    schedule.every(5).minutes.do(rotina_agendada)
     while True:
         schedule.run_pending()
         time.sleep(1)
